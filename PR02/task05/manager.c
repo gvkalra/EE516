cdc2227f924a9d639a2bda553baa74ee85550603
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-10-02 17:55:12 +0900

    task05 : comparison function

--- PR02/task05/manager.c
+++ PR02/task05/manager.c
@@ -1,11 +1,11 @@
 #include "manager.h"
+#include "sorting.h"
 #include "utils.h"
 
 #include <linux/task_io_accounting_ops.h>
 #include <linux/list.h>
 #include <linux/list_sort.h>
 #include <linux/slab.h>
-#include <linux/seq_file.h>
 
 #define INIT_MANAGER(e) \
 {											\
@@ -51,30 +51,78 @@ free_manager_node(struct manager_entry *entry)
 	kfree(entry);
 }
 
+static int
+sort_manager_entries(void *priv, struct list_head *a, struct list_head *b)
+{
+	struct manager_entry *entry_a, *entry_b;
+	int sort_order;
+
+	entry_a = list_entry(a, struct manager_entry, entries);
+	entry_b = list_entry(b, struct manager_entry, entries);
+	sort_order = get_current_sort_order();
+
+	switch (sort_order) {
+	case SORT_ORDER_VIRT:
+		return (entry_b->virt - entry_a->virt);
+	case SORT_ORDER_RSS:
+		return (entry_b->rss - entry_a->rss);
+	case SORT_ORDER_IO:
+		return (entry_b->total_io - entry_a->total_io);
+	case SORT_ORDER_PID:
+	default:
+		return (entry_a->pid - entry_b->pid);
+	}
+}
+
 int manager_init(void)
 {
-	struct task_struct *tsk;
+	struct task_struct *tsk, *t;
 	struct manager_entry *entry;
 	char name[TASK_COMM_LEN];
+	unsigned long virt;
+	long rss;
+	struct task_io_accounting acct;
 
 	dbg("");
 
+	/* Add to linked list */
 	for_each_process(tsk) {
 		dbg("name: [%s] pid: [%d]", get_task_comm(name, tsk),
 			task_pid_nr(tsk));
 
+		/* Calculate memory */
+		virt = 0;
+		rss = 0;
+		if (tsk->active_mm != NULL) {
+			virt = tsk->active_mm->total_vm;
+			rss += atomic_long_read(&tsk->active_mm->rss_stat.count[MM_FILEPAGES]);
+			rss += atomic_long_read(&tsk->active_mm->rss_stat.count[MM_ANONPAGES]);
+		}
+
+		/* Calculate I/O */
+		acct = tsk->ioac;
+		t = tsk;
+		task_io_accounting_add(&acct, &tsk->signal->ioac);
+		while_each_thread(tsk, t)
+			task_io_accounting_add(&acct, &t->ioac);
+
+		/* Allocate node */
 		entry = alloc_manager_node(task_pid_nr(tsk),
 			get_task_comm(name, tsk),
-			0, 0,
-			0, 0,
-			0);
+			virt, rss,
+			acct.read_bytes, acct.write_bytes,
+			(acct.read_bytes + acct.write_bytes));
 		if (entry == NULL) {
 			info("Skipping PID: [%d]", task_pid_nr(tsk));
 			continue;
 		}
 
+		/* Add to list */
 		list_add(&entry->entries, &manager_init_entry.entries);
 	}
+
+	/* Sort list */
+	list_sort(NULL, &manager_init_entry.entries, sort_manager_entries);
 
 	return 0;
 }
