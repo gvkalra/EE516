a7895877ec1ef3fc799e073600b70eda1ce96e50
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-10-10 14:38:19 +0900

    task03: finalize

--- PR02/task03/main.c
+++ PR02/task03/main.c
@@ -1,24 +1,101 @@
-#include "sequence.h"
 #include "utils.h"
 
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+#include <linux/mm_types.h>
 
 #define PROC_NAME "proc_memory"
 
 static struct proc_dir_entry *pl;
 
+/*
+Organization of task information in kernel:
+
+	struct task_struct {
+		...
+		pid_t pid;
+		...
+		struct list_head tasks;
+		...
+		char comm[TASK_COMM_LEN];
+		...
+		struct mm_struct *active_mm;
+	};
+
+	struct list_head {
+		 struct list_head *next, *prev;
+	};
+
+	struct mm_struct {
+		...
+		unsigned long total_vm;
+		...
+		struct mm_rss_stat rss_stat;
+	};
+
+	struct mm_rss_stat {
+		atomic_long_t count[NR_MM_COUNTERS];
+	};
+
+	 enum {
+		MM_FILEPAGES,  //Resident file mapping pages (Type of File Mapped Page)
+		MM_ANONPAGES,  //Resident anonymous pages (Type of Anonymous Page - Stack, Heap)
+		MM_SWAPENTS,   //Anonymous swap entries
+		MM_SHMEMPAGES, //Resident shared memory pages
+		NR_MM_COUNTERS
+	};
+
+	MM_FILEPAGES + MM_ANONPAGES = RSS Memory
+	total_vm = VIRT Memory
+*/
+
+static int
+pl_show(struct seq_file *m, void *v)
+{
+	struct task_struct *tsk;
+	char name[TASK_COMM_LEN];
+	unsigned long long virt = 0;
+	long long rss = 0;
+
+	/* header */
+	seq_printf(m, "PID       ProcessName         "
+		"VIRT(KB)            RSS Mem(KB)         \n");
+
+	/* Print name, PID, VIRT & RSS of each process */
+	for_each_process(tsk) {
+		virt = rss = 0;
+
+		/* It is possible for active_mm to be NULL.
+		 * In this case, we simply assume VIRT and RSS to be 0.
+		 * Ref: https://www.kernel.org/doc/Documentation/vm/active_mm.txt
+		 */
+		if (tsk->active_mm != NULL) {
+			/* VIRT memory (x4 because each page is 4 Bytes) */
+			virt = 4 * tsk->active_mm->total_vm;
+
+			/* RSS Memory */
+			rss += atomic_long_read(&tsk->active_mm->rss_stat.count[MM_FILEPAGES]);
+			rss += atomic_long_read(&tsk->active_mm->rss_stat.count[MM_ANONPAGES]);
+			rss *= 4; /* x4 because each page is 4 Bytes */
+		}
+
+		/* print information */
+		seq_printf(m, "%-10u%-20s%-20llu%-20llu\n",
+			task_pid_nr(tsk),
+			get_task_comm(name, tsk),
+			virt, rss);
+	}
+
+	return 0;
+}
+
 static int
 pl_open(struct inode *inode, struct file *file)
 {
-	struct seq_operations *sops;
 	dbg("");
-
-	/* initialize sequential file, register operations
-	 * Ref: https://www.kernel.org/doc/htmldocs/filesystems/API-seq-open.html
-	*/
-	sops = get_sequence_ops();
-	return seq_open(file, sops);
+	return single_open(file, pl_show, NULL);
 }
 
 /* file operations */
@@ -33,7 +110,7 @@ static struct file_operations fops = {
 	.llseek = seq_lseek,
 
 	/* free the structures associated with sequential file */
-	.release = seq_release,
+	.release = single_release,
 };
 
 static void
