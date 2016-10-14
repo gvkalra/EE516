83687ea83b242c700b6a3b7ac40cbb1fc8573113
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-10-14 16:17:43 +0900

    task03 : finalize

--- HW02/task03/read_write.c
+++ HW02/task03/read_write.c
@@ -26,7 +26,7 @@ sem_t mutex; /* initial value = 1; access control to 'rc' */
 sem_t db; /* initial value = 1; access control to database */
 int rc = 0; /* # of processes reading or wanting to */
 
-uint8_t buffer; /* 1 byte */
+uint8_t buffer = 0; /* 1 byte */
 
 int main(int argc, const char *argv[])
 {
@@ -48,15 +48,18 @@ int main(int argc, const char *argv[])
 		exit(1);
 	}
 
+	/* initialize rand() seed */
+	srand(time(NULL));
+
 	/* readers */
 	pthread_create(&threads[0], NULL, reader, NULL);
 	pthread_create(&threads[1], NULL, reader, NULL);
 	pthread_create(&threads[2], NULL, reader, NULL);
 
 	/* writers */
-	pthread_create(&threads[3], NULL, writer, NULL);
-	pthread_create(&threads[4], NULL, writer, NULL);
-	pthread_create(&threads[5], NULL, writer, NULL);
+	pthread_create(&threads[3], NULL, writer, (void *)1);
+	pthread_create(&threads[4], NULL, writer, (void *)2);
+	pthread_create(&threads[5], NULL, writer, (void *)3);
 
 	/* wait for threads to exit */
 	for (i = 0; i < 6; i++)
@@ -74,12 +77,40 @@ static void *
 reader(void *arg)
 {
 	dbg("");
+
+	while (sleep(rand() % 3) == 0) {
+		sem_wait(&mutex);
+		rc = rc + 1;
+		if (rc == 1)
+			sem_wait(&db);
+		sem_post(&mutex);
+
+		dbg("[R] :: [%ld] : %u", gettid(), buffer);
+
+		sem_wait(&mutex);
+		rc = rc - 1;
+		if (rc == 0)
+			sem_post(&db);
+		sem_post(&mutex);
+	}
+
 	return NULL;
 }
 
 static void *
 writer(void *arg)
 {
+	uint8_t data;
 	dbg("");
+
+	while (sleep(1) == 0) {
+		data = (uint8_t)(uintptr_t)arg;
+
+		sem_wait(&db);
+		buffer = data;
+		dbg("[W] :: [%ld] : %u", gettid(), buffer);
+		sem_post(&db);
+	}
+
 	return NULL;
 }
