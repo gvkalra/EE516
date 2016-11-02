a9110de3744793960bb40cb8b147d5f527815dea
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-11-02 13:06:05 +0900

    HW03: initial commit

---
+++ HW03/task01.c
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+/* Make your own program using:
+ * lseek()    : reposition read/write file offset
+ * unlink()   : delete a name and possibly the file it refers to
+ * fchmod()   : change permissions of a file
+ * fchown()   : change ownership of a file
+ * link()     : make a new name for a file (hard link)
+ * symlink()  : make a new name for a file (soft link)
+ * readlink() : read value of a symbolic link
+ * fstat()    : get file status
+ * readdir()  : read a directory
+ * getcwd()   : get current working directory
+*/
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
