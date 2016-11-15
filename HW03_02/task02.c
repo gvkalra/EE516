ea9b0096ac8e88f2051cef35cf29235774facdf6
Gaurav Kalra <gvkalra@kaist.ac.kr>
2016-11-15 14:33:09 +0900

    HW03_02 initial commit

---
+++ HW03_02/task02.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+
+#include <stdlib.h>
+
+/* Make your own program using:
+ * [1] malloc()
+ * [2] calloc()
+ * [3] realloc()
+ * [4] free()
+ * for allocating and freeing dynamic memory
+*/
+
+int main(int argc, const char *argv[])
+{
+	return 0;
+}
